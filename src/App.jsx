286ab6f3de7515a3e779547fadbbaa15f4c16a43
Shell Bryson <shell@shellbryson.com>
2023-05-24 18:56:35 +0100

    feat: convert to React app

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+import MastheadImage from './assets/masthead.png';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="sff">
+
+      <div className="sff-masthead">
+        <img src={MastheadImage} alt="Edinburgh SFF banner image containing a space scene and mysterious forest" />
+      </div>
+
+      <header className="sff-header">
+        <h1>Edinburgh SFF</h1>
+        <h2>The Science Fiction &amp; Fantasy Writing community</h2>
+        <h3>New writers, Critique and Events.</h3>
+      </header>
+
+      <div className="sff-content">
+        <p>For the latest information and events, find us on Mastodon <a rel="me" href="https://writing.exchange/@EdinburghSFF">@EdinburghSFF@writing.exchange</a>, Twitter <a href="https://twitter.com/edinburghsff">@edinburghsff</a> or drop into our <a href="https://discord.gg/5EaXDTwrEY">Discord</a> for a chat.</p>
+      </div>
+
+      <div className="sff-community">
+        <h2>Join our community</h2>
+        <p>A friendly community for writers of all levels, whether just starting out or published.</p>
+        <p><a href="https://discord.gg/5EaXDTwrEY">Discord</a></p>
+        <p><a rel="me" href="https://writing.exchange/@EdinburghSFF">Mastodon</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default App
