07f60b248004dba178c69f93ddd77a702f9739db
Shell Bryson <shell@shellbryson.com>
2023-06-09 17:51:13 +0100

    feat: map now supports filtering

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.5.0] - 2023-06-09
+
+Added:
+  - Maps: filter by type
+
 ## [1.4.1] - 2023-06-09
 
 Fixed:
--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -7,6 +7,12 @@ import GoogleMapReact from 'google-maps-react-markers';
 
 // MUI
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
+import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
+import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
+import LocalCafeOutlinedIcon from '@mui/icons-material/LocalCafeOutlined';
+import LocalLibraryOutlinedIcon from '@mui/icons-material/LocalLibraryOutlined';
 
 // Custom UI
 import MapPin from './MapPin';
@@ -21,9 +27,27 @@ const styleMap={
   overflow: "hidden",
 }
 
+const iconStyle = {
+  display: "block",
+  width: "2rem",
+  height: "2rem",
+  fontSize: "2rem",
+  borderRadius: "50%",
+  backgroundColor: "rgba(255, 255, 255, 0.75)"
+}
+
+const styleFilter = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "1rem",
+  marginTop: "2rem",
+}
+
 export default function MapView() {
 
   const [locations, setLocations] = useState([]);
+  const [filteredLocations, setFilteredLocations] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
 
   const mapRef = useRef(null)
@@ -37,11 +61,12 @@ export default function MapView() {
     querySnapshot.forEach((doc) => {
       l.push({
         ...doc.data(),
-        id: doc.id
+        id: doc.id,
+        hidden: false
       });
     });
-    console.log("ESFF: loaded locations", l);
     setLocations(l);
+    setFilteredLocations(l);
     setIsLoaded(true)
   }
 
@@ -62,29 +87,53 @@ export default function MapView() {
     zoom: 13
   }
 
+  const filterMap = (tag) => {
+    const filtered = tag ? locations.filter(entry => entry.tags.includes(tag)) : locations;
+    setFilteredLocations(filtered);
+  }
+
   return (
     <>
       { isLoaded &&
-        <Box style={styleMap}>
-          <GoogleMapReact
-            apiKey={import.meta.env.VITE_GOOGLEMAPS_API_KEY}
-            bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLEMAPS_API_KEY }}
-            onGoogleApiLoaded={onGoogleApiLoaded}
-            loadingContent={<Spinner />}
-            defaultCenter={defaultLocation.center}
-            defaultZoom={defaultLocation.zoom}>
+        <>
+          <Box sx={styleFilter}>
+            <IconButton aria-label="Venues" onClick={() => filterMap('Venue')}>
+              <StarOutlinedIcon style={ iconStyle } />
+            </IconButton>
+            <IconButton aria-label="Bookshops" onClick={() => filterMap('Bookshop')}>
+              <MenuBookOutlinedIcon style={ iconStyle } />
+            </IconButton>
+            <IconButton aria-label="Cafes" onClick={() => filterMap('Cafe')}>
+              <LocalCafeOutlinedIcon style={ iconStyle } />
+            </IconButton>
+            <IconButton aria-label="Libraries" onClick={() => filterMap('Library')}>
+              <LocalLibraryOutlinedIcon style={ iconStyle } />
+            </IconButton>
+            <IconButton aria-label="All" onClick={() => filterMap()}>
+              <PushPinOutlinedIcon style={ iconStyle } />
+            </IconButton>
+          </Box>
+          <Box style={styleMap}>
+            <GoogleMapReact
+              apiKey={import.meta.env.VITE_GOOGLEMAPS_API_KEY}
+              bootstrapURLKeys={{ key: import.meta.env.VITE_GOOGLEMAPS_API_KEY }}
+              onGoogleApiLoaded={onGoogleApiLoaded}
+              loadingContent={<Spinner />}
+              defaultCenter={defaultLocation.center}
+              defaultZoom={defaultLocation.zoom}>
 
-            {locations.map((place) => (
-              <MapPin
-                key={place.id}
-                tags={place.tags}
-                title={place.title}
-                lat={parseFloat(place.lat)}
-                lng={parseFloat(place.lng)}
-              />
-            ))}
-          </GoogleMapReact>
-        </Box>
+              {filteredLocations.map((place) => (
+                <MapPin
+                  key={place.id}
+                  tags={place.tags}
+                  title={place.title}
+                  lat={parseFloat(place.lat)}
+                  lng={parseFloat(place.lng)}
+                />
+              ))}
+            </GoogleMapReact>
+          </Box>
+        </>
       }
       { !isLoaded && <Spinner />}
     </>
