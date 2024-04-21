f4365aea51e7b8b988eb29ebbd1edec43372e60c
Shell Bryson <shell@shellbryson.com>
2024-04-21 22:59:10 +0100

    feat: home splash & animation experiment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,8 @@ import { useTheme } from '@mui/material/styles';
 import PlacesOfInterest from './panels/PlacesOfInterest';
 import UpComingEvents from './panels/UpComingEvents';
 import SearchPanel from './panels/SearchPanel';
+import Logo from './Logo';
+import Background from './Background';
 
 // Resources
 import 'css.gg/icons/scss/chevron-left.scss'
@@ -17,25 +19,49 @@ export default function Home({onSearchMap}) {
 
   const theme = useTheme();
 
-  const styleWelcome={
-    display: "block",
-    height: "100%",
-    overflow: "auto",
-    padding: "1rem",
-    maxWidth: "calc(300px - 2rem)",
-    color: theme.palette.brand.main,
+  const styles = {
+    home: {
+      display: "block",
+      height: "100%",
+      overflow: "auto",
+      padding: "0 1rem 1rem",
+      maxWidth: "calc(300px - 2rem)",
+      color: theme.palette.brand.main,
+    },
+    masthead: {
+      display: "flex",
+      flexDirection: "column",
+      position: "relative",
+      width: "100%",
+      height: "200px",
+      marginBottom: "1rem",
+    },
+    splash: {
+      position: "absolute",
+      top: "5%",
+      width: "100%",
+      zIndex: 2,
+    },
   }
 
   return (
-    <Box style={styleWelcome} className="sff-welcome">
+    <Box style={styles.home} className="sff-home">
+      <Box style={styles.masthead} className="sff-masthead">
+        <Background />
+        <Box elevation={1} style={styles.splash}>
+          <Box style={{ display: "flex", justifyContent: "center"}}>
+            <Logo />
+          </Box>
+          <Typography component="p" variant="p_small" sx={{textAlign: "center"}}>
+            Edinburgh SFF
+          </Typography>
+          <Typography component="p" variant="p_small" sx={{textAlign: "center"}}>
+            The Writers Hub
+          </Typography>
+        </Box>
+      </Box>
       <Typography component="p">
-        Edinburgh SFF
-      </Typography>
-      <Typography component="p">
-        Hub
-      </Typography>
-      <Typography component="p">
-        Edinburgh SFF is a vibrant community of Science Fiction and Fantasy writers in Scotland.
+        Explore the map to discover places to write, events and resources.
       </Typography>
       <PlacesOfInterest />
       <UpComingEvents />
