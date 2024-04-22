f77535162c83c2fa101556e592d48056d6ab98a0
Shell Bryson <shell@shellbryson.com>
2024-04-22 09:55:25 +0100

    clean up MapPanel

--- src/components/MapPanel.jsx
+++ src/components/MapPanel.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 
-// Context
+// Contexts
 import { useApp } from '../context/AppContext';
 
 // MUI
 import Box from '@mui/material/Box';
+
+// Icons
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import MenuIcon from '@mui/icons-material/Menu';
 
 // Custom UI
 import PageLayout from '../layouts/PageLayout';
 import Navigation from './Navigation';
 import Home from './Home';
 
-// Icons
-import 'css.gg/icons/scss/chevron-left.scss'
-import 'css.gg/icons/scss/chevron-right.scss'
-import 'css.gg/icons/scss/menu.scss'
-
-export default function MapPanel({onSearchMap}) {
+export default function MapPanel() {
 
   const location = useLocation();
   const {
@@ -31,73 +31,70 @@ export default function MapPanel({onSearchMap}) {
     location.pathname === '/' ? setIsExploded(false) : setIsExploded(true);
   }, [location]);
 
-  const stylePanel={
-    display: "flex",
-    flexDirection: "column",
-    position: "absolute",
-    transition: "clip-path 200ms",
-    left: "0",
-    top: "0",
-    bottom: "0",
-    backgroundColor: "rgb(0, 0, 0)",
-    zIndex: 1000,
-    clipPath: isExploded ? "polygon(0% 0%, 97% 0%, 100% 3%, 100% 100%, 100% 100%, 0% 100%)" : "none"
-  }
-
-  const stylePanelInterior={
-    transition: "width 200ms, max-width 200ms",
-    minWidth: "10px",
-    maxWidth: "700px",
-    width: isExploded ? "calc(100vw - 1rem)" : (isExpanded ? "300px" : "10px"),
-    overflow: "hidden",
-    height: "100vh",
-  }
-
-  const stylePanelContent={
-    display: isExpanded ? "flex" : "none",
-    flexDirection: "column",
-    transition: "width 200ms",
-    width: isExploded ? "100%" : "300px",
-    height: "100%",
-  }
-
-  const styleExpander={
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    top: "1rem",
-    right: isExpanded ? "calc(-2rem + 1px)" : "calc(-3rem + 1px)",
-    width: isExpanded ? "2rem" : "3rem",
-    height: "3rem",
-    backgroundColor: "rgb(0, 0, 0)",
-    color: "rgb(255, 255, 255)",
-    cursor: "pointer",
-    clipPath: "polygon(0% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 0% 100%)"
+  const styles = {
+    panel: {
+      display: "flex",
+      flexDirection: "column",
+      position: "absolute",
+      transition: "clip-path 200ms",
+      left: "0",
+      top: "0",
+      bottom: "0",
+      backgroundColor: "rgb(0, 0, 0)",
+      zIndex: 1000,
+      clipPath: isExploded ? "polygon(0% 0%, 97% 0%, 100% 3%, 100% 100%, 100% 100%, 0% 100%)" : "none"
+    },
+    interior: {
+      transition: "width 200ms, max-width 200ms",
+      minWidth: "10px",
+      maxWidth: "700px",
+      width: isExploded ? "calc(100vw - 1rem)" : (isExpanded ? "300px" : "10px"),
+      overflow: "hidden",
+      height: "100vh",
+    },
+    content: {
+      display: isExpanded ? "flex" : "none",
+      flexDirection: "column",
+      transition: "width 200ms",
+      width: isExploded ? "100%" : "300px",
+      height: "100%",
+    },
+    expander: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "absolute",
+      top: "1rem",
+      right: isExpanded ? "calc(-2rem + 1px)" : "calc(-3rem + 1px)",
+      width: isExpanded ? "2rem" : "3rem",
+      height: "3rem",
+      backgroundColor: "rgb(0, 0, 0)",
+      color: "rgb(255, 255, 255)",
+      cursor: "pointer",
+      clipPath: "polygon(0% 0%, 80% 0%, 100% 20%, 100% 80%, 80% 100%, 0% 100%)"
+    }
   }
 
   const handleExpanderClick = () => {
     setIsExpanded(!isExpanded);
   }
 
-  const isChildOfAdmin = location.pathname.startsWith('/admin');
-
   return (
-    <Box style={stylePanel} className="sff-panel">
+    <Box style={styles.panel} className="sff-panel">
       {!isExploded &&
-        <Box style={styleExpander} onClick={() => handleExpanderClick()}  className="sff-panel__expander">
+        <Box style={styles.expander} onClick={() => handleExpanderClick()}  className="sff-panel__expander">
           {isExpanded ?
-            <i className="gg-chevron-left"></i>
+            <ChevronLeftIcon />
           :
             <>
-              <i className="gg-menu"></i>
-              <i className="gg-chevron-right"></i>
+              <MenuIcon />
+              <ChevronRightIcon />
             </>
           }
         </Box>
       }
-      <Box style={stylePanelInterior} className="sff-panel__interior">
-        <Box style={stylePanelContent} className="sff-panel__content">
+      <Box style={styles.interior} className="sff-panel__interior">
+        <Box style={styles.content} className="sff-panel__content">
           <Navigation />
           { isExploded ? <PageLayout /> : <Home /> }
         </Box>
