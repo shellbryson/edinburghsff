d786c496d440ff40103635a65eddeffbb4c655b1
Shell Bryson <shell@shellbryson.com>
2024-04-25 14:37:03 +0100

    fix: spinner not in brand colours

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 
 // MUI Components
+import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 
-const spinnerContainerStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: "100%",
-  minHeight: "100%"
-}
-
 export default function Spinner() {
+  const theme = useTheme();
+
+  const style = {
+    container : {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: "100%",
+      minHeight: "100%",
+    },
+    spinner : {
+      color: theme.palette.brand.main,
+    }
+  }
+
   return (
-    <Box style={spinnerContainerStyle}>
-      <CircularProgress />
+    <Box style={style.container}>
+      <CircularProgress style={style.spinner}/>
     </Box>
   );
 };
