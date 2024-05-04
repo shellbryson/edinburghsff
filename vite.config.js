c2edced483655b50eef2aa15bd0dd55d71d09d2b
Shell Bryson <shell@shellbryson.com>
2024-05-04 12:00:52 +0100

    put back the vite conf

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  plugins: [react()]
 })
