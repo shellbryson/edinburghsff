f71e7832b6d36d982b7140c46a90363e1d28e709
Shell Bryson <shell@shellbryson.com>
2024-05-04 11:39:48 +0100

    experimental vite config to remove chunks

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
