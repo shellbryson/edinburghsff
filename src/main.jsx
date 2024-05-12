1ca92c1451ae28f4ab183e7c540b4a8830569c5e
Shell Bryson <shell@shellbryson.com>
2024-05-12 21:05:34 +0100

    feat: add Sentry

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: import.meta.env.REACT_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/edinburghsff.com/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+
 import { AuthContextProvider } from './context/AuthContext.jsx';
 import { AppContextProvider } from './context/AppContext';
 
