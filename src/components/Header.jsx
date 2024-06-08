c266e73a763a1d2ca0d445c44bb14b3f62f4427b
Shell Bryson <shell@shellbryson.com>
2024-06-08 23:32:02 +0100

    OG test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 export default function Header({meta}) {
+  const [metaData, setMetaData] = useState({});
+
+  useEffect(() => {
+    console.log(meta);
+    setMetaData(meta);
+  }, [meta?.title]);
 
   return (
     <Helmet>
-      <title>{`${meta.title} - Edinburgh SFF`}</title>
-      <meta property="og:title" content={meta.title} />
-      <meta property="og:description" content={meta.description} />
-      <meta property="og:image" content={meta?.image} />
+      <title>{`${metaData.title} - Edinburgh SFF`}</title>
+      <meta property="og:title" content={`${metaData.title}`} />
+      <meta property="og:description" content={metaData.description} />
+      <meta property="og:image" content={metaData?.image} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={window.location.href} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={`${meta.title} - Edinburgh SFF`} />
-      <meta name="twitter:description" content={meta.description} />
+      <meta name="twitter:title" content={`${metaData.title} - Edinburgh SFF`} />
+      <meta name="twitter:description" content={metaData.description} />
       <meta name="twitter:image" content="https://edinburghsff.com/TwitterCardMasthead.png" />
     </Helmet>
   );
