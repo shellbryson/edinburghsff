b0f146f2186952ea21c33fced6a97f92bfb497b2
Shell Bryson <shell@shellbryson.com>
2024-06-21 11:33:11 +0100

    fix: Sentry reporting issues in Dev (only care about prod)

--- src/main.jsx
+++ src/main.jsx
@@ -10,19 +10,21 @@ import ScrollToTop from './helpers/ScrollToTop.jsx';
 
 import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: "https://185898a833a620092430bdaaa3cb7eb9@o4507245141557248.ingest.de.sentry.io/4507245445775440",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: [
-    "https://.*\.edinburghsff.com/.*"
-  ],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+if (import.meta.env.VITE_VERCEL_ENV === "production") {
+  Sentry.init({
+    dsn: "https://185898a833a620092430bdaaa3cb7eb9@o4507245141557248.ingest.de.sentry.io/4507245445775440",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: [
+      "https://.*\.edinburghsff.com/.*"
+    ],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
